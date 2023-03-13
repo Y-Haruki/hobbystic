294749a596485a5575ab9b82bd1ee04ca0b43bf4
Y-Haruki <yamaharu05181412@gmail.com>
2023-03-13 11:38:05 +0900

    register機能

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,78 @@
 @extends('layouts.app')
-
+ 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+        <div class="col-md-6 hobbystic-container">
+            <h3 class="mt-3 mb-3 text-center hobbystic-login">新規会員登録</h3>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            <br>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <div class="form-group">
+                    <label for="name" class="col-md-5 col-form-label text-md-left">ユーザ名<span class="ml-1 hobbystic-require-input-label"><span class="hobbystic-require-input-label-text">必須</span></span></label>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    <div class="col-md-12">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror hobbystic-login-input " name="name" value="{{ old('name') }}" required autocomplete="name" autofocus >
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>氏名を入力してください</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="email" class="col-md-5 col-form-label text-md-left">メールアドレス<span class="ml-1 hobbystic-require-input-label"><span class="hobbystic-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-12">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror hobbystic-login-input" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>メールアドレスを入力してください</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password" class="col-md-5 col-form-label text-md-left">パスワード<span class="ml-1 hobbystic-require-input-label"><span class="hobbystic-require-input-label-text">必須</span></span></label>
+
+                    <div class="col-md-12">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror hobbystic-login-input" name="password" required autocomplete="new-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="password-confirm" class="col-md-5 col-form-label text-md-left">パスワード（確認）</label>
+
+                    <div class="col-md-12">
+                        <input id="password-confirm" type="password" class="form-control hobbystic-login-input" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                </div>
+
+                <div>
+                    <a class="btn btn-link mt-3 mb-1 d-flex justify-content-center hobbystic-login-text" href="{{ route('login') }}">
+                        既に登録済みですか？
+                    </a>
+                </div>
+
+                <div class="form-group hobbystic-button-container">
+                    <button type="submit" class="btn hobbystic-submit-button w-auto">
+                        会員登録
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
