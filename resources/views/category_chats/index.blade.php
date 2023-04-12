008fab4f11715765a6147c74183164589d65b597
Y-Haruki <yamaharu05181412@gmail.com>
2023-04-12 10:55:58 +0900

    categoryごとに表示（掲示板）

--- resources/views/category_chats/index.blade.php
+++ resources/views/category_chats/index.blade.php
@@ -16,9 +16,9 @@
       <button type="submit" class="px-2 py-1">作成</button>
     </div>
   </form>
-
   @foreach ($category_chats as $category_chat)
   <div>
+    @if ($category_chat->category_id === $category->id)
     <div class="d-flex">
       <div class="rounded-circle icon-image board-user-icon">
         @if ($category_chat->user->icon === 'default_icon')
@@ -30,6 +30,7 @@
       <p>{{ optional($category_chat->user)->name }}</p>
     </div>
     <div>{{ $category_chat->chat }}</div>
+    @endif
   </div>
   @endforeach
 </div>
